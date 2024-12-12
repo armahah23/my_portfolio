586c495fb8931cb60fe03ce8e7c55d64ec03537b
armahah23 <armahemp.off@gmail.com>
2024-12-12 11:47:16 +0530

    Update icon color in Skills component for improved visibility

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,7 +29,7 @@ const Skills = () => {
               viewport={{ once: false, amount: 0.7 }}
               className={`flex flex-col items-center p-4 shadow-lg rounded-lg ${getRandomGradient()}`}
             >
-              <FontAwesomeIcon icon={skill.icon} className="w-16 h-16 mb-4 text-gray-800" />
+              <FontAwesomeIcon icon={skill.icon} className="w-16 h-16 mb-4 text-gray-300" />
               <h3 className="text-xl font-semibold mb-2 text-gray-800">{skill.name}</h3>
               <div className="w-full bg-gray-200 rounded-full h-4 mb-4">
                 <div
